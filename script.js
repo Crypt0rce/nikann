3f4166561165d78c5a3d45e13504caeb293e127d
⛩﴾ 鬼 ﴿⛩ <69819862+Crypt0rce@users.noreply.github.com>
2025-03-07 15:21:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function updateCountdown() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementByльтакже "days").textContent = days;
-    document.getElementByльтакже "hours").textContent = hours;
-    document.getElementByльтакже "minutes").textContent = minutes;
-    document.getElementByльтакже "seconds").textContent = seconds;
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementBy이너 (minutes).textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
 }
 
 setInterval(updateCountdown, 1000);
